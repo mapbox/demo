7729e4b2701a7a810d9cf85a35842d67053b87d9
Tom MacWright <tom@macwright.org>
2011-11-01 13:16:24 -0400

    First commit.

---
+++ making_massredistrict/js/site.js
@@ -0,0 +1,38 @@
+$(window).load(function() {
+  $('textarea.js-snippet').each(function(a, b, elem) {
+      CodeMirror.fromTextArea(a, {
+              readOnly: 'nocursor',
+              mode: {name:'javascript'},
+              lineNumbers: true,
+              value: elem.text()
+          }
+      );
+  });
+  $('textarea.python-snippet').each(function(a, b, elem) {
+      CodeMirror.fromTextArea(a, {
+              readOnly: 'nocursor',
+              mode: {name:'python'},
+              lineNumbers: true,
+              value: elem.text()
+          }
+      );
+  });
+  $('textarea.carto-snippet').each(function(a, b, elem) {
+      CodeMirror.fromTextArea(a, {
+              readOnly: 'nocursor',
+              mode: {name:'carto', reference: ref},
+              lineNumbers: true,
+              value: elem.text()
+          }
+      );
+  });
+  $('textarea.html-snippet').each(function(a, b, elem) {
+      CodeMirror.fromTextArea(a, {
+              readOnly: 'nocursor',
+              mode: {name:'htmlmixed'},
+              lineNumbers: true,
+              value: elem.text()
+          }
+      );
+  });
+});
